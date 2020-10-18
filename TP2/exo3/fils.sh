f07dce479f715f93e1fbc70bd040e11aeaf30338
Zlatan Gojak <47949331+ZlatanGD@users.noreply.github.com>
2020-10-18 15:02:54 +0200

    Add files via upload

---
+++ TP2/exo3/fils.sh
@@ -0,0 +1,5 @@
+ #! /bin/bash
+
+echo "je suis $$ et mon père est $PPID";
+
+#Ceci est un commentaire dans fils.sh
---
+++ TP2/exo3/pere.sh
@@ -0,0 +1,9 @@
+ #! /bin/bash
+
+echo "Mon PID est : $$";
+for((i=0;i<10;i++))
+do
+./fils.sh
+done
+
+#Ceci est un commentaire dans pere.sh
